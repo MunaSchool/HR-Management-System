51e122e11af91a5f516fb94277ed648464332e59
ahmed69k <ahmedassem04@gmail.com>
2025-11-16 17:11:43 +0200

    delete everything

--- src/employee-profile/employee-profile.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { EmployeeProfileService } from './employee-profile.service';
-import { EmployeeProfileController } from './employee-profile.controller';
-
-@Module({
-  providers: [EmployeeProfileService],
-  controllers: [EmployeeProfileController]
-})
-export class EmployeeProfileModule {}
