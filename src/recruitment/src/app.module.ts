a61bd8b23c6b28a7ac62acf2054ddfd7229b76d4
LightGirlCyber <nour.m.elsisi@gmail.com>
2025-11-16 23:25:29 +0200

    edited app.module

--- src/recruitment/src/app.module.ts
+++ src/recruitment/src/app.module.ts
@@ -1,20 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import OnboardingModule from './onboarding/onboarding.module';
+import { AppService } from './app.service'; 
+import { OnboardingModule } from './onboarding/onboarding.module';
+import { OffboardingModule } from './offboarding/offboarding.module';
+import { RecruitmentModule } from './recruitment/recruitment.module';
 
 @Module({
   imports: [
-    // connect directly to your local Mongo for now
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/hr_recruitment_system'),
-
-    // your onboarding subsystem
     OnboardingModule,
-
     OffboardingModule,
-
     RecruitmentModule,
   ],
   controllers: [AppController],
