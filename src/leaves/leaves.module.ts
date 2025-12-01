fa5f1e6312b7446f1e526c06cc492fb189cc59be
Ahmed <ahmedassem04@Gmail.com>
2025-12-01 23:21:52 +0200

    full system

--- src/leaves/leaves.module.ts
+++ src/leaves/leaves.module.ts
@@ -1,30 +1,45 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { LeavesController } from './leaves.controller';
 import { LeavesService } from './leaves.service';
-import { MongooseModule } from '@nestjs/mongoose';
-import { LeaveType, LeaveTypeSchema } from './models/leave-type.schema';
-import { LeaveRequest, LeaveRequestSchema } from './models/leave-request.schema';
-import { LeavePolicy, LeavePolicySchema } from './models/leave-policy.schema';
-import { LeaveEntitlement, LeaveEntitlementSchema } from './models/leave-entitlement.schema';
+
+// MODELS
 import { LeaveCategory, LeaveCategorySchema } from './models/leave-category.schema';
-import { LeaveAdjustment, LeaveAdjustmentSchema } from './models/leave-adjustment.schema';
-import { Calendar, CalendarSchema} from './models/calendar.schema';
-import { Attachment,AttachmentSchema } from './models/attachment.schema';
-import { EmployeeProfileModule } from '../employee-profile/employee-profile.module';
-import { TimeManagementModule } from '../time-management/time-management.module';
+import { LeaveType, LeaveTypeSchema } from './models/leave-type.schema';
+import { LeavePolicy, LeavePolicySchema } from './models/leave-policy.schema';
+import { Calendar, CalendarSchema } from './models/calendar.schema';
+
+import { ApprovalWorkflow, ApprovalWorkflowSchema } from './models/approval-workflow.schema';
+import { LeaveEntitlement, LeaveEntitlementSchema } from './models/leave-entitlement.schema';
+import { LeavePaycodeMapping, LeavePaycodeMappingSchema } from './models/leave-paycode-mapping.schema';
+import { LeaveRequest, LeaveRequestSchema } from './models/leave-request.schema';
+import { Attachment, AttachmentSchema } from './models/attachment.schema';
+import { EmployeeProfile, EmployeeProfileSchema } from '../employee-profile/models/employee-profile.schema';
+
+// PHASE 3 MODULES
+import { LeaveTrackingModule } from './leave-tracking/leave-tracking.module';
+import { LeaveRequestModule } from './leave-request/leave-request.module';
 
 @Module({
-  imports:[MongooseModule.forFeature([{name:LeaveType.name,schema:LeaveTypeSchema},
-    {name:LeaveRequest.name, schema: LeaveRequestSchema},
-    {name:LeavePolicy.name, schema:LeavePolicySchema},
-    {name:LeaveEntitlement.name, schema:LeaveEntitlementSchema},
-    {name: LeaveCategory.name, schema:LeaveCategorySchema},
-    {name: LeaveAdjustment.name, schema:LeaveAdjustmentSchema},
-    {name:Calendar.name, schema:CalendarSchema},
-    {name:Attachment.name, schema: AttachmentSchema}
-  ]),EmployeeProfileModule,TimeManagementModule],
+  imports: [
+    MongooseModule.forFeature([
+      { name: LeaveCategory.name, schema: LeaveCategorySchema },
+      { name: LeaveType.name, schema: LeaveTypeSchema },
+      { name: LeavePolicy.name, schema: LeavePolicySchema },
+      { name: Calendar.name, schema: CalendarSchema },
+      { name: ApprovalWorkflow.name, schema: ApprovalWorkflowSchema },
+      { name: LeaveEntitlement.name, schema: LeaveEntitlementSchema },
+      { name: LeavePaycodeMapping.name, schema: LeavePaycodeMappingSchema },
+      { name: LeaveRequest.name, schema: LeaveRequestSchema },
+      { name: Attachment.name, schema: AttachmentSchema },
+      { name: EmployeeProfile.name, schema: EmployeeProfileSchema },
+    ]),
+    LeaveTrackingModule,
+    LeaveRequestModule,
+  ],
   controllers: [LeavesController],
   providers: [LeavesService],
-  exports:[LeavesService]
+  exports: [LeavesService],
 })
 export class LeavesModule {}
