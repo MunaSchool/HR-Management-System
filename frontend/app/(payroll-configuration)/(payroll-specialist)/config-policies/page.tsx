4f5acfa8b128ac3cf40c6b267a2257b41bbb28d2
salma-sherif-06 <salma.mohammed@student.giu-uni.de>
2025-12-12 18:06:52 +0200

    7aga

---
+++ frontend/app/(payroll-configuration)/(payroll-specialist)/config-policies/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+//import { useAuth } from '@/hooks/useAuth';
+
+
+export default function ConfigPoliciesPage() {
+
+}
