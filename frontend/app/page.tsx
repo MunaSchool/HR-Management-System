112c82c844315bc949871dccf7b69bc64c4226b7
Habiba-Walid1 <habibaw006@gmail.com>
2025-12-17 17:28:58 +0300

    just changed root redirect from /home to /login

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from 'next/navigation';
 
 export default function Home() {
-  redirect('/home');
+  redirect('/login');
 
   // This component will not render, as the redirect happens on the server.
 }
